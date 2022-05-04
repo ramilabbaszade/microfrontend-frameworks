39a0992d540d7d7c921f25397e1b57a31ad932a8
ramilabbaszade <abbasovramil65@gmail.com>
2022-05-04 15:32:17 +0400

    initial

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+// Mount function to startup the app
+const mount = (el) => {
+  ReactDOM.render(<App/>, el);
+};
+
+// If we are in development and isolation call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container and we should export the mount funtion
+export { mount };
