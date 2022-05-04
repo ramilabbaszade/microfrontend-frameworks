30dd1387fe0bc20fef9bfbf6c6c7c9c21ec08244
ramilabbaszade <abbasovramil65@gmail.com>
2022-05-04 16:30:11 +0400

    connect marketing to container

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
@@ -11,9 +12,16 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename:'remoteEntry.js',
+      exposes:{
+        './MarketingApp':'./src/bootstrap'
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-    }),
+    })
   ],
 };
 
